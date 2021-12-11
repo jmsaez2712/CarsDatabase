4cd8128d5bbdf56aa2dbf83421b25f85ee0a4bea
TheSintBex <jmsaez2712@ieszaidinvergeles.org>
2021-12-11 16:55:54 +0100

    Commit

--- app/src/main/java/com/saezgarcia/carsdatabase/view/activity/MainActivity.java
+++ app/src/main/java/com/saezgarcia/carsdatabase/view/activity/MainActivity.java
@@ -1,43 +1,20 @@
 package com.saezgarcia.carsdatabase.view.activity;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
-import androidx.navigation.NavHostController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.Manifest;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.saezgarcia.carsdatabase.R;
-import com.saezgarcia.carsdatabase.model.entity.Car;
-import com.saezgarcia.carsdatabase.model.entity.CarType;
-import com.saezgarcia.carsdatabase.model.entity.Type;
-import com.saezgarcia.carsdatabase.view.fragments.AddCarFragment;
-import com.saezgarcia.carsdatabase.view.fragments.CarsListFragment;
-import com.saezgarcia.carsdatabase.viewmodel.CarViewModel;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,15 +24,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbarFragment);
 
         setSupportActionBar(toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
     }
 
 
--- app/src/main/java/com/saezgarcia/carsdatabase/view/fragments/CarsListFragment.java
+++ app/src/main/java/com/saezgarcia/carsdatabase/view/fragments/CarsListFragment.java
@@ -1,25 +1,34 @@
 package com.saezgarcia.carsdatabase.view.fragments;
 
+import android.bluetooth.le.ScanSettings;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.saezgarcia.carsdatabase.R;
 import com.saezgarcia.carsdatabase.model.entity.CarType;
+import com.saezgarcia.carsdatabase.view.activity.MainActivity;
 import com.saezgarcia.carsdatabase.view.adapter.CarAdapter;
 import com.saezgarcia.carsdatabase.viewmodel.CarViewModel;
 
@@ -29,7 +38,10 @@ import java.util.List;
 public class CarsListFragment extends Fragment {
 
     private MaterialCardView card;
-    private ActionMode actionMode;
+    private FloatingActionButton fab;
+    private Toolbar toolbar;
+    private AppBarLayout appBarLayout;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
     private CarViewModel cmv;
 
     public CarsListFragment() {
@@ -38,51 +50,102 @@ public class CarsListFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         super.onCreate(savedInstanceState);
-
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_cars_list, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        initRecycler(view);
+        initComp(view);
+        showTitleCollapse(view);
+        toolbarListener();
+    }
+
+    private void initRecycler(View view){
         RecyclerView rvCar = view.findViewById(R.id.rv);
         rvCar.setLayoutManager(new LinearLayoutManager(getContext()));
         cmv = new ViewModelProvider(requireActivity()).get(CarViewModel.class);
         CarAdapter carAdapter = new CarAdapter(getContext());
-
-        card = view.findViewById(R.id.card);
         rvCar.setAdapter(carAdapter);
 
         LiveData<List<CarType>> listCarType = cmv.getAllCars();
         listCarType.observe(getViewLifecycleOwner(), cars -> {
             carAdapter.setCarList(cars);
         });
+    }
 
-        FloatingActionButton fab = view.findViewById(R.id.fabAdd);
+    private void initComp(View view){
+        card = view.findViewById(R.id.card);
+
+        toolbar = view.findViewById(R.id.toolbarFragment);
+        toolbar.inflateMenu(R.menu.menu);
+
+        fab = view.findViewById(R.id.fabAdd);
         fab.setOnClickListener((View v) -> {
-                NavHostFragment.findNavController(this).navigate(R.id.action_FirstFragment_to_SecondFragment);
+            NavHostFragment.findNavController(this).navigate(R.id.action_FirstFragment_to_SecondFragment);
         });
+    }
 
+    private void toolbarListener(){
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.types_opt:
+                        NavHostFragment.findNavController(CarsListFragment.this).navigate(R.id.action_FirstFragment_to_typeFragment);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
 
-        }
+    private void showTitleCollapse(View view){
+        collapsingToolbarLayout = view.findViewById(R.id.collapsingAppbar);
+        appBarLayout = view.findViewById(R.id.appbarFragment);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.BaseOnOffsetChangedListener() {
+            boolean isDown = true;
+            int scrollRange = -1;
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle("Car database");
+                    isDown = true;
+                } else if(isDown) {
+                    collapsingToolbarLayout.setTitle(" ");//careful there should a space between double quote otherwise it wont work
+                    isDown = false;
+                }
+            }
+        });
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((MainActivity)getActivity()).getSupportActionBar().hide();
+    }
 
-
-    /*@Override
-    public void onPrepareOptionsMenu(@NonNull Menu menu) {
-        super.onPrepareOptionsMenu(menu);
-        MenuItem item = menu.findItem(R.id.action_delete);
-        MenuItem item2 = menu.findItem(R.id.action_edit);
-        item.setVisible(card.isChecked() && card != null);
-        item2.setVisible(card.isChecked() && card != null);
-    }*/
-
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((MainActivity)getActivity()).getSupportActionBar().show();
+    }
 
 }
